956869b6175f350da21e2a4ffc5aa1cca3bd1d1e
Oquindo <12414140@benilde.edu.ph>
2026-05-22 19:54:40 +0800

    Update profile with skills list and graduation calculation

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ is_student = True
 print("Name:", name)
 print("Age:", age)
 print("Student Status:", is_student)
+
+
+#Appending new collection structures and calculations
+skills = ["Python", "Git", "HTML"]
+years_to_graduation = 4 - 1.5
+
+print("Skills:", skills)
+print("Years left to graduate:", years_to_graduation)
